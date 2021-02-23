a8ca576c38615a054f6e489335df1c4bbaa33a18
Sunjianwang <859684581@qq.com>
2021-02-23 18:07:24 +0800

    接入QQ登陆

--- note_android/Note_Android/app/src/main/java/com/example/note_android/login/LoginActivity.kt
+++ note_android/Note_Android/app/src/main/java/com/example/note_android/login/LoginActivity.kt
@@ -1,22 +1,29 @@
 package com.example.note_android.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.note_android.MainActivity
-import com.example.note_android.login.bean.UserInfo
 import com.example.note_android.R
 import com.example.note_android.annotation.Page
 import com.example.note_android.databinding.ActivityLoginBinding
+import com.example.note_android.login.QQLogin.MyIUiListener
+import com.example.note_android.login.bean.UserInfo
 import com.example.note_android.util.ActivityUtil
+import com.tencent.tauth.Tencent
+
 
 @Page(name = "登录页")
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
     var loginBinding:ActivityLoginBinding? = null
+    var mTencent: Tencent? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         loginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+        mTencent = Tencent.createInstance("101936988", this.getApplicationContext());
         initView()
         initListener()
     }
@@ -31,13 +38,28 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         loginBinding?.iconQqLogin?.setOnClickListener(this)
     }
 
+    private fun QQLogin(){
+        if (!mTencent!!.isSessionValid) {
+            mTencent!!.login(this, "all", MyIUiListener(this))
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Tencent.onActivityResultData(requestCode,resultCode,data,MyIUiListener(this))
+    }
+
     override fun onClick(v: View?) {
         when(v?.id){
             R.id.login_button ->{
                 ActivityUtil.get()?.goActivityKill(this,MainActivity::class.java)
             }
             R.id.icon_qq_login ->{
-                
+                QQLogin()
             }
         }
     }
