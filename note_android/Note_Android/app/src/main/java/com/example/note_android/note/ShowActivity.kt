e4c263ea18677848843f28106bb712b499dd3c13
Sunjianwang <859684581@qq.com>
2021-03-01 16:34:21 +0800

    优化个人信息页面,创建笔记展示页

---
+++ note_android/Note_Android/app/src/main/java/com/example/note_android/note/ShowActivity.kt
@@ -0,0 +1,12 @@
+package com.example.note_android.note
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.note_android.R
+
+class ShowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show)
+    }
+}
